8476d00e1539208f0791398a4b6f85aebad3d759
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-16 17:13:48 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,3 @@
-
 import os
-os.system("git clone repo && cd repo name && python3 -m userbot")
+os.system("git clone https://github.com/ComradeDear/HUNTERBOT.git && cd repo HUNTERBOT && python3 -m userbot")
 print("sed got error report in support chat")
